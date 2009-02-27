38fa8d597c92b1fbf36c89988a2e48e935b36386
Merlise Clyde <clyde@stat.duke.edu>
2009-02-27 00:00:00 +0000

    version 0.1

---
+++ src/mem.c
@@ -0,0 +1,143 @@
+/* Modified to be compatible with R memory allocation */
+#include <stdio.h>
+#include <stdlib.h>
+#include <R.h>
+
+double *vecalloc(int nr)
+{
+  double *x;
+  x=(double *) R_alloc(nr,sizeof(double));
+  return x;
+}
+
+long *lvecalloc(int nr)
+{
+  long *x;
+  x=(long *) R_alloc((unsigned) nr,sizeof(long));
+  return x;
+}
+
+
+
+float *fvecalloc(int nr)
+{
+  float *x;
+  x=(float *) R_alloc((unsigned) nr,sizeof(float));
+  return x;
+}
+
+int *ivecalloc(int nr)
+{
+  int *x;
+  x= (int *) R_alloc( nr, sizeof(int));
+  return x;
+}
+
+/*
+double **matallocold(int nr, int nc)
+{
+int k;
+double **x;
+x= Calloc( nr, double *);
+for (k=0;k<nr;k++){
+  x[k]= Calloc(nc, double);
+  }
+return x;
+}
+*/
+
+float **fmatalloc(int nr, int nc)
+{
+int k;
+float **x;
+x=(float **) R_alloc((unsigned) nr, sizeof(float *));
+
+for (k=0;k<nr;k++){
+  x[k]=(float *) R_alloc((unsigned) nc, sizeof(float));
+}
+return x;
+}
+
+double **matalloc(int nr, int nc)
+{
+int k;
+double **x;
+x=(double **) R_alloc((unsigned) nr, sizeof(double *));
+
+for (k=0;k<nr;k++){
+  x[k]=(double *) R_alloc((unsigned) nc, sizeof(double));
+}
+return x;
+}
+
+int  **imatalloc(int nr, int nc)
+{
+int k;
+int **x;
+x = (int **) R_alloc(nr, sizeof(int *));
+for (k=0;k<nr;k++){
+  x[k] = (int *) R_alloc(nc, sizeof(int));
+}
+return x;
+}
+
+unsigned char  **cmatalloc(int nr, int nc)
+{
+int k;
+unsigned char  **x;
+x = (unsigned char **) R_alloc(nr, sizeof(unsigned char *));
+for (k=0;k<nr;k++){
+  x[k] = (unsigned char  *) R_alloc(nc, sizeof(unsigned char));
+}
+return x;
+}
+
+/* 
+unsigned char **cmatallocold(int nr, int nc)
+{
+int k;
+unsigned char **x;
+x= Calloc(nr, unsigned char *);
+for (k=0;k<nr;k++){
+  x[k]=  Calloc(nc, unsigned char);
+}
+return x;
+}
+
+*/
+
+void  freemat(double **mat, int  nr)
+{
+int k;
+
+for (k=0;k<nr;k++){
+  Free(mat[k]);
+  }
+Free(mat);
+
+}
+
+void  freeimat(int **mat, int  nr)
+{
+int k;
+
+for (k=0;k<nr;k++){
+  Free(mat[k]);
+  }
+Free(mat);
+
+}
+
+void  freechmat(unsigned char **mat, int  nr)
+{
+int k;
+
+for (k=0;k<nr;k++){
+  Free(mat[k]);
+  }
+Free(mat);
+}
+
+
+
+
